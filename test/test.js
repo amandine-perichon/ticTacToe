cc35bac525a6644b8352cfd32ba1821bf580b1ed
EDA <eda@mac-mini-2.local>
2016-07-21 16:53:27 +1200

    returns empty board

--- test/test.js
+++ test/test.js
@@ -5,3 +5,13 @@ test('initial', function (t) {
   t.plan(1)
   t.equal('a', 'a')
 })
+
+test('create an empty board', function (t){
+  //Arrange
+  var expected = [["-","-","-"],["-","-","-"],["-","-","-"]]
+  //Act
+  var actual = game()
+  //Assert
+  t.deepEqual(actual, expected)
+  t.end()
+})
